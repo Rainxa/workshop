32ef18387f24c6a1be532a3825484841528f8ec7
Rainxa <75529978+Rainxa@users.noreply.github.com>
2024-03-09 22:35:20 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/*function onLoad() {
+    document.getElementById("mainDiv").classList.remove("intro")
+    setTimeout(() => { 
+        hasSkipped = true 
+        document.getElementById("mainDiv").style.pointerEvents = ''
+        
+    }, 5000)
+}
+let hasSkipped = false
+function skipIntro() {
+    if (!hasSkipped) {
+        var d = document.getElementById("mainDiv")
+        d.style.transition = '0.5s'
+        d.style.pointerEvents = ''
+        d.classList.add("intro")
+        setTimeout(() => d.classList.remove("intro"), 0)
+        hasSkipped = true
+    }
+}*/
